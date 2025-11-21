3d9e41ba2f34f9751adb59016028fd3a216a0cd3
LuisAlberto2304 <0323105921@ut-tijuana.edu.mx>
2025-11-20 22:59:28 -0800

    cambio de nextconfig

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ const isDev = process.env.NODE_ENV !== "production";
 
 const baseCSP =
   "default-src 'self'; " +
-  "script-src 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.googletagmanager.com https://www.google-analytics.com https://js.stripe.com https://www.paypal.com https://www.sandbox.paypal.com https://connect.facebook.net; " +
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.vercel.app https://*.google.com https://*.gstatic.com https://www.googletagmanager.com https://www.google-analytics.com https://js.stripe.com https://www.paypal.com https://www.sandbox.paypal.com; " +
   "style-src 'self' 'unsafe-inline'; " +
-  "img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.googleusercontent.com https://*.ggpht.com https://images.unsplash.com https://picsum.photos https://d1fufvy4xao6k9.cloudfront.net https://etianguis.s3.us-east-2.amazonaws.com https://medusa-public-images.s3.eu-west-1.amazonaws.com https://www.facebook.com; " +
-  "connect-src 'self' https://apis.google.com https://accounts.google.com https://www.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://api.stripe.com https://www.paypal.com https://www.sandbox.paypal.com https://www.googletagmanager.com https://www.google-analytics.com https://*.algolia.net https://*.algolianet.com https://countriesnow.space https://caissoned-uncorrelative-dedra.ngrok-free.app https://graph.facebook.com; " +
-  "frame-src 'self' https://etianguis-fa446.firebaseapp.com https://accounts.google.com https://js.stripe.com https://www.paypal.com https://www.sandbox.paypal.com; " +
+  "img-src 'self' blob: data: https://*; " +
+  "connect-src 'self' https://*; " +
+  "frame-src 'self' https://*.vercel.app https://accounts.google.com https://js.stripe.com https://www.paypal.com https://www.sandbox.paypal.com; " +
   "object-src 'none'; base-uri 'self'; form-action 'self';";
+
 
 const securityHeaders = [
   // COOP/COEP — permite popups durante desarrollo
@@ -25,10 +26,11 @@ const securityHeaders = [
       ]),
 
   // CSP
-  {
-    key: "Content-Security-Policy",
-    value: baseCSP,
-  },
+// {
+//   key: "Content-Security-Policy",
+//   value: baseCSP,
+// },
+
 
   { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
   { key: "X-Frame-Options", value: "DENY" },
